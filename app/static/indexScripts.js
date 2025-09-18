4244872ecd0c1d89f981b488ce4a61e718e17f13
Bhuvantej123 <81451571+Bhuvantej123@users.noreply.github.com>
2025-09-18 14:48:24 +0530

    index page done

---
+++ app/static/indexScripts.js
@@ -0,0 +1,4 @@
+document.getElementById("uploadBtn").addEventListener("click", () => {
+  alert("Redirecting to PDF upload page...");
+  window.location.href = "upload.html"; // You can create this later
+});
