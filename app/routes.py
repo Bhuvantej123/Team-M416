f9f0a400cd6522fbf96da192bdc6149e1f2fada6
pithambar20 <pithambar20@gmail.com>
2025-09-18 14:57:13 +0530

    upload routes

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,8 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     return render_template("index.html")
+
+@main.route("/upload") 
+def upload():
+    return render_template("upload.html")
+
